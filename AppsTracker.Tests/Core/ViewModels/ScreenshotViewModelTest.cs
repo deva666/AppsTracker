52cb40bb4a1cff974d03711fc05ceddf66ac1ee6
deva <madevcic@gmail.com>
2014-11-18 13:36:15 +0100

    missing files added

---
+++ AppsTracker.Tests/Core/ViewModels/ScreenshotViewModelTest.cs
@@ -0,0 +1,49 @@
+﻿#region Licence
+/*
+  *  Author: Marko Devcic, madevcic@gmail.com
+  *  Copyright: Marko Devcic, 2014
+  *  Licence: http://creativecommons.org/licenses/by-nc-nd/4.0/
+ */
+#endregion
+
+using AppsTracker.DAL.Service;
+using AppsTracker.Pages.ViewModels;
+using AppsTracker.Tests.Fakes.Service;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppsTracker.Tests.Core.ViewModels
+{
+    [TestClass]
+    public class ScreenshotViewModelTest
+    {
+        private volatile bool _loaded = false;
+
+        [TestInitialize]
+        public void Init()
+        {
+            if (!ServiceFactory.ContainsKey<IAppsService>())
+                ServiceFactory.Register<IAppsService>(() => new AppsServiceMock());
+        }
+
+        [TestMethod]
+        public void TestScreenshotLoader()
+        {
+            var vm = new Data_screenshotsViewModel();
+            vm.LogList.PropertyChanged += LogList_PropertyChanged;
+            var r = vm.LogList.Result;
+
+            while (_loaded == false)
+            {
+            }
+
+            Assert.IsNotNull(vm.LogList.Result, "Content not loaded");
+        }
+
+        void LogList_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Result")
+                _loaded = true;
+        }
+    }
+}
---
+++ AppsTracker.Tests/DAL/Service/ServiceFactoryTest.cs
@@ -0,0 +1,51 @@
+﻿#region Licence
+/*
+  *  Author: Marko Devcic, madevcic@gmail.com
+  *  Copyright: Marko Devcic, 2014
+  *  Licence: http://creativecommons.org/licenses/by-nc-nd/4.0/
+ */
+#endregion
+
+
+using System;
+
+using AppsTracker.DAL.Service;
+using AppsTracker.Tests.Fakes.Service;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppsTracker.Tests.DAL.Service
+{
+    [TestClass]
+    public class ServiceFactoryTest
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            if (ServiceFactory.ContainsKey<IAppsService>() == false)
+                ServiceFactory.Register<IAppsService>(() => new AppsServiceMock());
+            if (ServiceFactory.ContainsKey<IChartService>() == false)
+                ServiceFactory.Register<IChartService>(() => new ChartServiceMock());
+        }
+        [TestMethod]
+        public void TestRegisterMethod()
+        {
+            Assert.IsInstanceOfType(ServiceFactory.Get<IAppsService>(), typeof(AppsServiceMock), "IAppsService type don't match");
+            Assert.IsInstanceOfType(ServiceFactory.Get<IChartService>(), typeof(ChartServiceMock), "IChartService type don't match");
+        }
+
+        [TestMethod]
+        public void TestDoubleRegistration()
+        {
+            try
+            {
+                ServiceFactory.Register<IAppsService>(() => new AppsServiceMock());
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(InvalidOperationException), "Exception types don't match");
+                Assert.IsInstanceOfType(ServiceFactory.Get<IAppsService>(), typeof(AppsServiceMock), "Failed get after double registration");
+            }
+        }
+    }
+}
