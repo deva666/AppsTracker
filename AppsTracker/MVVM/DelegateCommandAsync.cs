a4bff5517524ae62751f8369f5a1b180053b7add
Marko Devcic <devcic@WORKDEV50.telecomsoftware.com>
2015-02-26 08:58:39 +0100

    missing files

---
+++ AppsTracker/MVVM/DelegateCommandAsync.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AppsTracker.MVVM
+{
+   public class DelegateCommandAsync : ICommand
+   {
+      private Func<Task> _delegate;
+      private Func<object, Task> _parameterizedDelegate;
+
+      public DelegateCommandAsync(Func<Task> @delegate)
+      {
+         _delegate = @delegate;
+      }
+
+      public DelegateCommandAsync(Func<object, Task> parameterizedDelegate)
+      {
+         _parameterizedDelegate = parameterizedDelegate;
+      }
+
+      public bool CanExecute(object parameter)
+      {
+         return true;
+      }
+
+      public event EventHandler CanExecuteChanged;
+
+      public async void Execute(object parameter)
+      {
+         if (_parameterizedDelegate != null)
+            await _parameterizedDelegate(parameter);
+         else
+            await _delegate();
+      }
+   }
+}
