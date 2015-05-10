31b57449f19bf009fac1c9db5dec27f9e854fdeb
deva <madevcic@gmail.com>
2015-05-10 12:55:14 +0200

    missing project files

---
+++ AppsTracker/Views/Settings/AppLimitsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AppsTracker.Widgets.SettingsViews
+{
+    public partial class AppLimitsView : UserControl
+    {
+        public AppLimitsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
