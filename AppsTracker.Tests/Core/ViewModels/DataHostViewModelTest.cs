7393da22f84b9e25c732c1abe7e1d686cc87959b
Marko - laptop <madevcic@gmail.com>
2015-07-03 20:32:43 +0200

    added datahost view model tests

---
+++ AppsTracker.Tests/Core/ViewModels/DataHostViewModelTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using AppsTracker.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppsTracker.Tests.Core.ViewModels
+{
+    [TestClass]
+    public class DataHostViewModelTest : TestMockBase
+    {
+        private DataHostViewModel CreateViewModel()
+        {
+            var viewModel = new DataHostViewModel(GetAppDetailsVMFactory(),
+                                                  GetScreenshotsVMFactory(),
+                                                  GetDaySummaryVMFactory());
+            return viewModel;
+        }
+
+        [TestMethod]
+        public void TestNavigateToAppDetails()
+        {
+            var viewModel = CreateViewModel();
+            viewModel.GoToAppDetailsCommand.Execute(null);
+            Assert.IsInstanceOfType(viewModel.SelectedChild, typeof(AppDetailsViewModel));
+        }
+
+        [TestMethod]
+        public void TestNavigateToDaySummary()
+        {
+            var viewModel = CreateViewModel();
+            viewModel.GoToDaySummaryCommand.Execute(null);
+            Assert.IsInstanceOfType(viewModel.SelectedChild, typeof(DaySummaryViewModel));
+        }
+
+        [TestMethod]
+        public void TestNavigateToScreenshots()
+        {
+            var viewModel = CreateViewModel();
+            viewModel.GoToScreenshotsCommand.Execute(null);
+            Assert.IsInstanceOfType(viewModel.SelectedChild, typeof(ScreenshotsViewModel));
+        }
+    }
+}
