734c4192c08265daf8e53d52bc4044919102f371
Marko - laptop <madevcic@gmail.com>
2016-09-25 15:37:13 +0200

    class rename

--- src/AppsTracker.Domain/Apps/AppCategoryModel.cs
+++ src/AppsTracker.Domain/Apps/AppCategoryModel.cs
@@ -1,6 +1,20 @@
-﻿namespace AppsTracker.Domain.Apps
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace AppsTracker.Domain.Apps
 {
     public sealed class AppCategoryModel
     {
+        public ObservableCollection<AppModel> ObservableApplications { get; set; }
+
+        public AppCategoryModel()
+        {
+        }
+
+        public int ID { get; private set; }
+
+        public string Name { get; private set; }
+
+        public ICollection<AppModel> Applications { get; set; }
     }
 }
